338ee45aa5c0fb1e1a01e9143cf2f189fb739e4e
Chat6ot <charlie60312@icloud.com>
2023-05-10 13:30:21 +0800

    modify

--- store/urls.py
+++ store/urls.py
@@ -2,7 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('__debug__/', include('debug_toolbar.urls')),
     path("", views.store, name="store"),
     path("category/<slug:category_slug>/", views.store, name="products_by_category"),
     path("category/<slug:category_slug>/<slug:product_slug>/", views.product_detail, name="product_detail"),
