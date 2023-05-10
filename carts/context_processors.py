8c0a5075f74419e963c57b7e40fd09ab6db8aaee
Chat6ot <charlie60312@icloud.com>
2023-05-10 16:06:56 +0800

    OperationalError fixed

--- carts/context_processors.py
+++ carts/context_processors.py
@@ -14,8 +14,9 @@ def counter(request):
                 cart_items = CartItem.objects.all().filter(user=request.user)
             else:
                 cart_items = CartItem.objects.all().filter(cart=cart[:1])
-            for cart_item in cart_items:
-                cart_count += cart_item.quantity
+            if cart_items:
+                for cart_item in cart_items:
+                    cart_count += cart_item.quantity
         except Cart.DoesNotExist:
             cart_count = 0
 
