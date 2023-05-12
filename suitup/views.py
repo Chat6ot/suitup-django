e9262f556b2e4ba290376c395f5924deedf25df2
Chat6ot <charlie60312@icloud.com>
2023-05-12 13:10:19 +0800

    index top 8 product

--- suitup/views.py
+++ suitup/views.py
@@ -3,10 +3,11 @@ from store.models import Product
 
 
 def home(request):
-    products = Product.objects.all().filter(is_available=True)
+    products = Product.objects.all().filter(is_available=True).order_by("stock")
+    top_products = list(products[:8])
 
     context = {
-        "products": products,
+        "top_products": top_products,
     }
 
     return render(request, "index.html", context)
