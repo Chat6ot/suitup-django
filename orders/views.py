cff08a65ec58a35e4b21885fc4acffa78ab0a781
Chat6ot <charlie60312@icloud.com>
2023-05-14 22:13:30 +0800

    place_order refactor

--- orders/views.py
+++ orders/views.py
@@ -17,56 +17,51 @@ def place_order(request, total=0, quantity=0):
         return redirect("store")
 
     delivery_fee = 100
+
     for item in cart_items:
         total += (item.product.price * item.quantity)
         quantity += item.quantity
+
     grand_total = total + delivery_fee
 
     if request.method == "POST":
         form = OrderForm(request.POST)
         if form.is_valid():
             # Store all the billing information inside Order table
-            data = Order()
-            data.user = current_user
-            data.email = current_user.email
-            data.recipient_name = form.cleaned_data["recipient_name"]
-            data.phone = form.cleaned_data["phone"]
-            data.address_line = form.cleaned_data["address_line"]
-            data.district = form.cleaned_data["district"]
-            data.city = form.cleaned_data["city"]
-            data.payment_method = form.cleaned_data["payment_method"]
-            data.order_note = form.cleaned_data["order_note"]
-            data.order_total = grand_total
-            data.delivery_fee = delivery_fee
-            data.ip = request.META.get("REMOTE_ADDR")
-            data.is_ordered = True
-            data.save()
+            data = form.cleaned_data
+            order = Order.objects.create(
+                user=current_user,
+                email=current_user.email,
+                recipient_name=data["recipient_name"],
+                phone=data["phone"],
+                address_line=data["address_line"],
+                district=data["district"],
+                city=data["city"],
+                payment_method=data["payment_method"],
+                order_note=data["order_note"],
+                order_total=grand_total,
+                delivery_fee=delivery_fee,
+                ip=request.META.get("REMOTE_ADDR"),
+                is_ordered=True
+            )
 
             # Generate order number
-            yr = int(datetime.date.today().strftime("%Y"))
-            dt = int(datetime.date.today().strftime("%d"))
-            mt = int(datetime.date.today().strftime("%m"))
-            d = datetime.date(yr, mt, dt)
-            current_date = d.strftime("%Y%m%d")
-            order_number = current_date + str(data.id)
-            data.order_number = order_number
-            data.save()
-
-            order = Order.objects.get(user=current_user, order_number=order_number)
+            current_date = datetime.date.today().strftime("%Y%m%d")
+            order_number = current_date + str(order.id)
+            order.order_number = order_number
+            order.save()
 
             for item in cart_items:
-                order_product = OrderProduct()
-                order_product.order_id = order.id
-                order_product.user_id = current_user.id
-                order_product.product_id = item.product_id
-                order_product.quantity = item.quantity
-                order_product.product_price = item.product.price
-                order_product.ordered = True
-                order_product.save()
+                order_product = OrderProduct.objects.create(
+                    order=order,
+                    user=current_user,
+                    product=item.product,
+                    quantity=item.quantity,
+                    product_price=item.product.price,
+                    ordered=True
+                )
 
-                cart_item = CartItem.objects.get(id=item.id)
-                product_variation = cart_item.variations.all()
-                order_product = OrderProduct.objects.get(id=order_product.id)
+                product_variation = item.variations.all()
                 order_product.variations.set(product_variation)
                 order_product.save()
 
@@ -74,7 +69,7 @@ def place_order(request, total=0, quantity=0):
                 product.stock -= item.quantity
                 product.save()
 
-            CartItem.objects.filter(user=current_user).delete()
+            cart_items.delete()
 
             mail_subject = "Thank you for your order!"
             message = render_to_string("orders/order_received_email.html", {
@@ -85,7 +80,7 @@ def place_order(request, total=0, quantity=0):
             send_email = EmailMessage(mail_subject, message, to=[to_email])
             send_email.send()
 
-            order_products = OrderProduct.objects.filter(order_id=data.id)
+            order_products = OrderProduct.objects.filter(order=order)
 
             context = {
                 "order": order,
