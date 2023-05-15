ae2de3eeb65d1c8cf9238ec70fd4bcd39e4fc840
Chat6ot <charlie60312@icloud.com>
2023-05-15 11:12:18 +0800

    break to smaller function

--- orders/views.py
+++ orders/views.py
@@ -26,59 +26,14 @@ def place_order(request, total=0, quantity=0):
 
     if request.method == "POST":
         form = OrderForm(request.POST)
+        ip = request.META.get("REMOTE_ADDR")
         if form.is_valid():
-            # Store all the billing information inside Order table
-            data = form.cleaned_data
-            order = Order.objects.create(
-                user=current_user,
-                email=current_user.email,
-                recipient_name=data["recipient_name"],
-                phone=data["phone"],
-                address_line=data["address_line"],
-                district=data["district"],
-                city=data["city"],
-                payment_method=data["payment_method"],
-                order_note=data["order_note"],
-                order_total=grand_total,
-                delivery_fee=delivery_fee,
-                ip=request.META.get("REMOTE_ADDR"),
-                is_ordered=True
-            )
-
-            # Generate order number
-            current_date = datetime.date.today().strftime("%Y%m%d")
-            order_number = current_date + str(order.id)
-            order.order_number = order_number
-            order.save()
-
-            for item in cart_items:
-                order_product = OrderProduct.objects.create(
-                    order=order,
-                    user=current_user,
-                    product=item.product,
-                    quantity=item.quantity,
-                    product_price=item.product.price,
-                    ordered=True
-                )
-
-                product_variation = item.variations.all()
-                order_product.variations.set(product_variation)
-                order_product.save()
-
-                product = Product.objects.get(id=item.product_id)
-                product.stock -= item.quantity
-                product.save()
-
+            order = _create_order(form, current_user, grand_total, delivery_fee, ip)
+            _create_order_product(order, current_user, cart_items)
+            _update_product_stock(cart_items)
+            _send_order_confirmation_mail(order, current_user)
+            # clear cart items by user
             cart_items.delete()
-
-            mail_subject = "Thank you for your order!"
-            message = render_to_string("orders/order_received_email.html", {
-                "user": current_user,
-                "order": order,
-            })
-            to_email = current_user.email
-            send_email = EmailMessage(mail_subject, message, to=[to_email])
-            send_email.send()
 
             order_products = OrderProduct.objects.filter(order=order)
 
@@ -96,3 +51,61 @@ def place_order(request, total=0, quantity=0):
             print(form.errors)
 
     return redirect("checkout")
+
+
+def _create_order(form, user, grand_total, delivery_fee, ip):
+    data = form.cleaned_data
+    order = Order.objects.create(
+        user=user,
+        email=user.email,
+        recipient_name=data["recipient_name"],
+        phone=data["phone"],
+        address_line=data["address_line"],
+        district=data["district"],
+        city=data["city"],
+        payment_method=data["payment_method"],
+        order_note=data["order_note"],
+        order_total=grand_total,
+        delivery_fee=delivery_fee,
+        ip=ip,
+        is_ordered=True
+    )
+    current_date = datetime.date.today().strftime("%Y%m%d")
+    order_number = current_date + str(order.id)
+    order.order_number = order_number
+    order.save()
+    return order
+
+
+def _create_order_product(order, user, cart_items):
+    for item in cart_items:
+        order_product = OrderProduct.objects.create(
+            order=order,
+            user=user,
+            product=item.product,
+            quantity=item.quantity,
+            product_price=item.product.price,
+            ordered=True
+        )
+
+        product_variation = item.variations.all()
+        order_product.variations.set(product_variation)
+        order_product.save()
+
+
+def _update_product_stock(cart_items):
+    for item in cart_items:
+        product = Product.objects.get(id=item.product_id)
+        product.stock -= item.quantity
+        product.save()
+
+
+def _send_order_confirmation_mail(order, user):
+    mail_subject = "Thank you for your order!"
+    message = render_to_string("orders/order_received_email.html", {
+        "user": user,
+        "order": order,
+    })
+    to_email = user.email
+    send_email = EmailMessage(mail_subject, message, to=[to_email])
+    send_email.send()
